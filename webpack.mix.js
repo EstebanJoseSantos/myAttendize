6cf697d97ecb2b153a079bf6e07f72bcc3611a2a
Sam Bell <me@sam-bell.com>
2020-04-04 12:23:18 +0100

    Icon font copy

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,10 @@ let assets = 'public/assets/';
 Mix.manifest.refresh = _ => void 0;
 
 mix
+    .copyDirectory(
+        assets + "src/sass/icons/iconfont/fonts",
+        assets + "dist/css/fonts"
+    )
     .js(assets + "src/js/app.js", assets + "dist/js/app.js")
     .sass(assets + "src/sass/application.scss", assets + "dist/css")
     .options({
