2ae8746320283536c7b21b1307db8b3d1b98c042
EstebanJoseSantos <lpolanco@kantera.com>
2020-04-28 23:22:26 -0400

    Added links to duplicate, restore, archive

--- resources/views/ManageOrganiser/Partials/EventPanel.blade.php
+++ resources/views/ManageOrganiser/Partials/EventPanel.blade.php
@@ -1,5 +1,5 @@
 <div class="panel panel-success event">
-    <div class="panel-heading" data-style="background-color: {{{$event->bg_color}}};background-image: url({{{$event->bg_image_url}}}); background-size: cover;">
+    <div class="panel-heading" data-style="background-color: {{{ $event->bg_color }}};background-image: url({{{$event->bg_image_url}}}); background-size: cover;" {{$event->deleted_at==null?'':'style=background-color:gray;'  }}   >
         <div class="event-date">
             <div class="month">
                 {{strtoupper(explode("|", trans("basic.months_short"))[$event->start_date->format('n')])}}
@@ -11,7 +11,7 @@
         <ul class="event-meta">
             <li class="event-title">
                 <a title="{{{$event->title}}}" href="{{route('showEventDashboard', ['event_id'=>$event->id])}}">
-                    {{{ Str::limit($event->title, $limit = 75, $end = '...') }}}
+                    {{{ str_limit($event->title, $limit = 75, $end = '...') }}}
                 </a>
             </li>
             <li class="event-organiser">
@@ -25,14 +25,14 @@
         <ul class="nav nav-section nav-justified mt5 mb5">
             <li>
                 <div class="section">
-                    <h4 class="nm">{{ $event->tickets->sum('quantity_sold') }}</h4>
+                    <h4 class="nm">{{$event->tickets->sum('quantity_sold')}}</h4>
                     <p class="nm text-muted">@lang("Event.tickets_sold")</p>
                 </div>
             </li>
 
             <li>
                 <div class="section">
-                    <h4 class="nm">{{ $event->getEventRevenueAmount()->display() }}</h4>
+                    <h4 class="nm">{{{money($event->sales_volume + $event->organiser_fees_volume, $event->currency)}}}</h4>
                     <p class="nm text-muted">@lang("Event.revenue")</p>
                 </div>
             </li>
@@ -41,17 +41,22 @@
     <div class="panel-footer">
         <ul class="nav nav-section nav-justified">
             <li>
-                <a href="{{route('showEventCustomize', ['event_id' => $event->id])}}">
+                <a href="{{ $event->deleted_at==null? route('showEventCustomize',['event_id' => $event->id]) : '#' }}" {{ $event->deleted_at==null? '':'disabled' }}>
                     <i class="ico-edit"></i> @lang("basic.edit")
                 </a>
             </li>
             <li>
-                <a href="{{route('showCreateEvent', ['event_id' => $event->id])}}">
-                    <i class="ico-copy"></i> @lang("basic.clone")
+                <a href="{{ $event->deleted_at==null? route('duplicateEvent', ['event_id' => $event->id ] ) : '#' }}" {{ $event->deleted_at==null?'':'disabled' }} >
+                    <i class="ico-copy"></i> @lang("basic.duplicate")
                 </a>
             </li>
             <li>
-                <a href="{{route('showEventDashboard', ['event_id' => $event->id])}}">
+                <a href="{{  $event->deleted_at==null? route('archiveEvent',['event_id' => $event->id]) : route('restoreEvent', ['event_id' => $event->id]) }}">
+                    <i class="{{ $event->deleted_at==null?'ico-trash':'ico-undo' }}" > </i> {{ $event->deleted_at==null? '  '.trans("basic.archive") : '  '.trans("basic.restore") }}
+                </a>
+            </li>
+            <li>
+                <a href="{{ $event->deleted_at==null? route('showEventDashboard', ['event_id' => $event->id]): '#' }}" {{ $event->deleted_at==null?'':'disabled' }} >
                     <i class="ico-cog"></i> @lang("basic.manage")
                 </a>
             </li>
